8f2c36acd5069ad98f670b3e64e5a9c8f20c8668
Vassily Krasnov <mykrasnov@gmail.com>
2016-05-07 15:57:37 +0300

    first commit

---
+++ .enb/make.js
@@ -0,0 +1,101 @@
+var techs = {
+        // essential
+        fileProvider: require('enb/techs/file-provider'),
+        fileMerge: require('enb/techs/file-merge'),
+
+        // optimization
+        borschik: require('enb-borschik/techs/borschik'),
+
+        // css
+        stylus: require('enb-stylus/techs/stylus'),
+
+        // js
+        browserJs: require('enb-js/techs/browser-js'),
+
+        // bemtree
+        // bemtree: require('enb-bemxjst/techs/bemtree'),
+
+        // bemhtml
+        bemhtml: require('enb-bemxjst/techs/bemhtml'),
+        bemjsonToHtml: require('enb-bemxjst/techs/bemjson-to-html')
+    },
+    enbBemTechs = require('enb-bem-techs'),
+    levels = [
+        { path: 'libs/bem-core/common.blocks', check: false },
+        { path: 'libs/bem-core/desktop.blocks', check: false },
+        { path: 'libs/bem-components/common.blocks', check: false },
+        { path: 'libs/bem-components/desktop.blocks', check: false },
+        { path: 'libs/bem-components/design/common.blocks', check: false },
+        { path: 'libs/bem-components/design/desktop.blocks', check: false },
+        'common.blocks',
+        'desktop.blocks'
+    ];
+
+module.exports = function(config) {
+    var isProd = process.env.YENV === 'production';
+
+    config.nodes('*.bundles/*', function(nodeConfig) {
+        nodeConfig.addTechs([
+            // essential
+            [enbBemTechs.levels, { levels: levels }],
+            [techs.fileProvider, { target: '?.bemjson.js' }],
+            [techs.borschik, { sourceTarget: '?.bemjson.js', destTarget: '?.bemjson.borschik.js', freeze: true, minify: false }],
+            [enbBemTechs.bemjsonToBemdecl, { source: '?.bemjson.borschik.js' }],
+            [enbBemTechs.deps],
+            [enbBemTechs.files],
+
+            // css
+            [techs.stylus, {
+                target: '?.css',
+                sourcemap: false,
+                autoprefixer: {
+                    browsers: ['ie >= 10', 'last 2 versions', 'opera 12.1', '> 2%']
+                }
+            }],
+
+            // bemtree
+            // [techs.bemtree, { sourceSuffixes: ['bemtree', 'bemtree.js'] }],
+
+            // bemhtml
+            [techs.bemhtml, { sourceSuffixes: ['bemhtml', 'bemhtml.js'] }],
+
+            // html
+            [techs.bemjsonToHtml, { bemjsonFile: '?.bemjson.borschik.js' }],
+
+            // client bemhtml
+            [enbBemTechs.depsByTechToBemdecl, {
+                target: '?.bemhtml.bemdecl.js',
+                sourceTech: 'js',
+                destTech: 'bemhtml'
+            }],
+            [enbBemTechs.deps, {
+                target: '?.bemhtml.deps.js',
+                bemdeclFile: '?.bemhtml.bemdecl.js'
+            }],
+            [enbBemTechs.files, {
+                depsFile: '?.bemhtml.deps.js',
+                filesTarget: '?.bemhtml.files',
+                dirsTarget: '?.bemhtml.dirs'
+            }],
+            [techs.bemhtml, {
+                target: '?.browser.bemhtml.js',
+                filesTarget: '?.bemhtml.files',
+                sourceSuffixes: ['bemhtml', 'bemhtml.js']
+            }],
+
+            // js
+            [techs.browserJs, { includeYM: true }],
+            [techs.fileMerge, {
+                target: '?.js',
+                sources: ['?.browser.js', '?.browser.bemhtml.js']
+            }],
+
+            // borschik
+            [techs.borschik, { source: '?.html', target: '?.borschik.html', freeze: true }],
+            [techs.borschik, { source: '?.js', target: '?.min.js', minify: isProd }],
+            [techs.borschik, { source: '?.css', target: '?.min.css', minify: isProd }]
+        ]);
+
+        nodeConfig.addTargets([/* '?.bemtree.js', */ '?.borschik.html', '?.min.css', '?.min.js']);
+    });
+};
